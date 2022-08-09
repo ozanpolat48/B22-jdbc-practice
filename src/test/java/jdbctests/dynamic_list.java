f8d58e6e0fa4c40f8d37ee903babca5342c06e30
ozanpolat48 <mrozanpolat@gmail.com>
2022-08-09 17:28:55 +0300

    Initial commit

---
+++ src/test/java/jdbctests/dynamic_list.java
@@ -0,0 +1,68 @@
+package jdbctests;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class dynamic_list {
+
+    String dbUrl = "jdbc:oracle:thin:@3.83.131.158:1521:XE";
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+
+
+    @Test
+    public void test2() throws SQLException {
+
+
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("SELECT first_name,last_name,salary,job_id\n" +
+                "from employees\n" +
+                "where rownum <6");
+
+        //in order to get column names we need resultsetmetadata
+        ResultSetMetaData rsdm = resultSet.getMetaData();
+
+        //list of maps to keep all information
+        List<Map<String,Object>> queryData = new ArrayList<>();
+
+        //number of columns
+        int colCount = rsdm.getColumnCount();
+
+        //loop through each row
+        while (resultSet.next()){
+
+            Map<String,Object> row = new HashMap<>();
+
+            //some code to fill the dynamically
+            for (int i = 1; i <= colCount ; i++) {
+
+                row.put(rsdm.getColumnName(i),resultSet.getObject(i));
+
+            }
+
+
+            //add ready map row to the list
+            queryData.add(row);
+
+        }
+
+        //print each row inside the list
+        for (Map<String, Object> row : queryData) {
+            System.out.println(row.toString());
+        }
+
+
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+}
